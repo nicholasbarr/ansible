b6bf5ae0c018f68cc4fdeeb0911f353e5293dbe2
Nic Barr <theothernic@outlook.com>
2019-08-15 04:23:01 -0230

    adding mirrorlist update

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,15 @@
 #! /usr/bin/env bash
 
+SCRIPT_DL_URL="https://www.archlinux.org/mirrorlist/?country=US";
+
+
+# get our copy of the mirrorlist.
+
+echo "Downloading a copy of the custom mirrorlist...";
+curl -sSL ${SCRIPT_DL_URL} >> mirrorlist.txt;
+sed -i -e 's/#Server/Server/g' mirrorlist.txt;
+mv mirrorlist.txt /etc/pacman.d/mirrorlist;
+
 sudo pacman -Syyu --noconfirm;
 sudo pacman -S --noconfirm git ansible;
 
